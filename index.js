b27a4da20a3af4a2f8e4b3e41ed88e3e875cb4f9
Mike Wilcox <anm9tr@gmail.com>
2018-07-16 07:05:09 -0500

    forgot THE main file

---
+++ index.js
@@ -0,0 +1,126 @@
+const webpack = require("webpack");
+const WebpackDevServer = require("webpack-dev-server");
+const serve = require('webpack-serve');
+const path = require('path');
+const fs = require('fs');
+const html = require('./config/html.plugin');
+const assets = require('./config/assets.plugin');
+//
+const args = require('minimist')(process.argv.slice(2));
+const localOptions = require(args.config);
+const isDev = args.mode === 'development';
+localOptions.projectRoot = process.cwd();
+
+
+function run (options, callback) {
+
+	// const filepath = path.resolve(localOptions.root, localOptions.output, localOptions.html.filename);
+	// console.log('filepath', filepath);
+	// return
+
+	const webpackConfig = require('./config/webpack.config');
+	const serverConfig = require('./config/server.config');
+	const serveConfig = require('./config/serve.config');
+	const webpackOptions = webpackConfig(options);
+
+	console.log('webpackOptions', webpackOptions);
+
+	if (args.mode === 'development') {
+
+
+		if (options.useServe) {
+			// doesn't work.
+			webpackOptions.serve = serveConfig(options);
+			serve({
+				config: webpackOptions
+			}).then((e) => {
+				console.log('You have been SERVED');
+			});
+
+		} else {
+
+			// Based on
+			// Script to launch webpack dev server
+			// https://gist.github.com/michaelrambeau/b04f83ef16fc78feee09
+
+			const svr = serverConfig(options);
+
+			// needed for HMR
+			WebpackDevServer.addDevServerEntrypoints(webpackOptions, svr);
+
+			const compiler = webpack(webpackOptions);
+			const port = options.port;
+			const server = new WebpackDevServer(compiler, svr);
+			server.listen(port, 'localhost', function (err) {
+				if (err) {
+					console.log(err);
+				}
+				console.log('WebpackDevServer listening at localhost:', port);
+			});
+		}
+
+
+	} else {
+		webpack(webpackConfig(options), (err, stats) => {
+			if (err || stats.hasErrors()) {
+				// Handle errors here
+				console.error('****ERROR****', err);
+				const info = stats.toJson();
+
+				if (stats.hasErrors()) {
+					console.error(stats.toJson().errors[0]);
+				}
+			} else {
+				console.log('webpack runner done processing');
+				if (callback) {
+					callback();
+				}
+			}
+		});
+	}
+}
+
+const build1 = `
+*********
+BUILD ONE
+*********
+`;
+
+const build2 = `
+*********
+BUILD TWO
+*********
+`;
+
+if (isDev) {
+	run(localOptions);
+} else if (localOptions.ieOnly || !localOptions.ie) {
+	run(localOptions, () => {
+		let content = html.read(localOptions);
+		content = assets.toFile(content, [], localOptions);
+		html.write(content, localOptions);
+		assets.flush();
+	});
+} else {
+	// modules/nomodules build step
+	// first run modern browser build
+	console.log(build1);
+	localOptions.ie = false;
+	run(localOptions, () => {
+
+		let content = html.read(localOptions);
+		const moduleScripts = assets.fromFile(content);
+		assets.cache(localOptions);
+
+		// now run legacy browser build
+		console.log(build2);
+		localOptions.ie = true;
+		run(localOptions, () => {
+			let content = html.read(localOptions);
+			content = assets.toFile(content, moduleScripts, localOptions);
+			html.write(content, localOptions);
+			assets.flush();
+		});
+	});
+}
+
